76d0f6bc2b48fe2a50cc7b926f41aafe1a72d75a
Dyego dos Santos Almeida <dyegocm@gmail.com>
2020-06-01 18:14:31 -0300

    Tela de Login

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php 
 
+session_start();
+
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -18,10 +21,38 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
     
+	User::verifyLogin();
+
 	$page = new PageAdmin();
 	$page->setTpl('index');
 });
 
+$app->get('/admin/login', function() {
+
+	//O header e o footer não é padrão na pagina de Login, por isso precisamos desativá-los
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function() {
+
+	User::login($_POST["login"], $_POST["password"]);
+	header("Location: /admin");
+	exit;
+});
+
+$app->get('/admin/logout', function () {
+
+	User::logout();
+	header("Location: /admin/login");
+	exit;
+
+});
+
 $app->run();
 
- ?>
+?>
