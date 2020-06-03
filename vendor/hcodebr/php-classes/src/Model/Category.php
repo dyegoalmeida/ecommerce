09c4862ef910a00c333850626b27c87ff453f194
Dyego dos Santos Almeida <dyegocm@gmail.com>
2020-06-03 18:43:19 -0300

    Site - Categorias

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -26,6 +26,8 @@ class Category extends Model {
 
 		$this->setData($results[0]);
 
+		Category::updateFile();
+
 	}
 
 	public function get($idcategory){
@@ -45,6 +47,26 @@ class Category extends Model {
 		$sql->query("DELETE FROM TB_CATEGORIES WHERE IDCATEGORY = :IDCATEGORY", [
 				':IDCATEGORY'=>$this->getidcategory()
 		]);
+
+		Category::updateFile();
+	}
+
+	public static function updateFile(){
+
+		$categories = Category::listAll();
+
+		$html = [];
+
+		foreach ($categories as $row) {
+			array_push($html, '<li><a href="/categories/'.$row['idcategory'].'">'.
+				       $row['descategory'].'</a></li>');
+		}
+
+		/*
+		Salva o conteúdo no arquivo categories-menu.html
+		Função Implode converte um array em uma string. Precisou para passar como parâmetro.
+		*/
+		file_put_contents($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR . "categories-menu.html", implode('', $html));
 	}
 
 }
---
+++ views-cache/categories-menu.a13d4be72968581a9c627ea1b69a1acf.rtpl.php
@@ -0,0 +1,1 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/8">Feminino</a></li><li><a href="/categories/7">Masculino</a></li><li><a href="/categories/6">Smartwatch</a></li><li><a href="/categories/9">Unissex</a></li>
